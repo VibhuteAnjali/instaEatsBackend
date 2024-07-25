e1f9dac8edf139249727c3a221e6ec4190615fdf
Anjali Vibhute <101789772+VibhuteAnjali@users.noreply.github.com>
2024-07-25 20:28:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,18 +15,18 @@ const app = express();
 const port = 3000;
 // const storage = new Storage();
 export const getGCPCredentials = () => {
-  // for Vercel, use environment variables
   return process.env.GCP_PRIVATE_KEY
     ? {
         credentials: {
           client_email: process.env.GCP_SERVICE_ACCOUNT_EMAIL,
-          private_key: process.env.GCP_PRIVATE_KEY,
+          private_key: process.env.GCP_PRIVATE_KEY.replace(/\\n/g, '\n'), // Replace escaped newlines
         },
         projectId: process.env.GCP_PROJECT_ID,
       }
-      // for local development, use gcloud CLI
     : {};
 };
+
+
 const bucketName = process.env.GCLOUD_STORAGE_BUCKET;
 
 app.use(cors());
